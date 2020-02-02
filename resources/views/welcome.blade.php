a0a36ac348fe38ca98e95eb10b1fe8c02b1405e4
aleksandar <aleksandar.ivanovski123@gmail.com>
2020-02-02 01:18:26 +0100

    Heroku commit w/ front-end improvements;

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -83,18 +84,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    BookLibrary
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/Aleksandar1932/BookLibrary/blob/master/README.md"><i class="fas fa-info"></i> About</a>
+                    <a href="https://github.com/Aleksandar1932"><i class="fas fa-user-cog"></i> Developer</a>
+                    <a href="https://github.com/Aleksandar1932/BookLibrary"><i class="fab fa-github"></i> Codebase</a>
                 </div>
             </div>
         </div>
